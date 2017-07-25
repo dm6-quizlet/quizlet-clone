d312b7b2fcbc7d7dfc76aee4b53443f70cbb8b4d
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-07-25 14:11:18 -0500

    created navbar (logged out) without functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Route, Link, Switch, Redirect} from 'react-router-dom'
 import Nav from './components/Nav/Nav'
-import Splash from './components/Splash/Splash'
+// import Splash from './components/Splash/Splash'
 import './App.css';
 
 class App extends Component {
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -6,7 +6,30 @@ import './Nav.css'
 class Nav extends Component {
   render() {
     return (
-      <h1 className="container">Hello Nav!<button className="btn btn-primary">Hi</button></h1>
+      <nav className="Nav">
+        <div className="container-fluid">
+          <a id="quizlet-logo">Quizlet</a>
+          <div className="nav-search">
+            <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
+            <span>Search</span>
+          </div>
+          <span className="separator">|</span>
+
+          <div className="nav-create">
+            <span className="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+            <span>Create</span>
+          </div>
+
+          <div className="nav-login">
+            <ul>
+              <li id="navbar-login">Log In</li>
+              <li><button type="button" className="btn btn-default navbar-btn">Sign Up</button></li>
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+
     )
   }
 }
