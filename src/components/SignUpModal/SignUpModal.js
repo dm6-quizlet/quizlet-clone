54c161eeb1f05e5b273a28ea977926b0c249442b
stence299 <stence299@gmail.com>
2017-07-26 12:19:23 -0500

    made the frame of the sign up modal

---
+++ src/components/SignUpModal/SignUpModal.js
@@ -0,0 +1,43 @@
+import React, { Component }from "react";
+
+export class SignUpModal extends Component {
+    render() {
+        return (
+            <div className="UI-Modal-Box">
+                <div className="Sign-Up-Modal-Box">
+                    <div>Something</div> {/* Sign up with Google */}
+                    <div></div> {/* Sign up with Facebook */}
+                    <form>
+                        <div> {/*  */}
+                            <div></div> {/* Month input */}
+                            <div></div> {/* Day input */}
+                            <div></div> {/* Year input */}
+                            <div></div> {/* onHover all ages */}
+                        </div>
+                        <label> {/* Username input */}
+                            <div>
+                                <input></input>
+                            </div>
+                            <span></span>
+                        </label>
+                        <label> {/* Email input */}
+                            <div>
+                                <input></input>
+                            </div>
+                            <span></span>
+                        </label>
+                        <label> {/* Password input */}
+                            <div>
+                                <input></input>
+                            </div>
+                            <span></span>
+                        </label>
+                        <div></div> {/* Terms of Service Checkbox */}
+                        <button></button> {/* Sign up button */}
+                        <div></div>
+                    </form>
+                </div>
+            </div>    
+        )
+    }
+}
